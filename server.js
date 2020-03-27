a7a5e6ae7d36eb11549497b9d028a5a7bcba0326
Vihar Desu <vihardesu@gmail.com>
2020-03-26 23:27:41 -0400

    prep for remote

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 console.log('master branch modified locally')
 console.log('hey')
 console.log('creating a difference')
-console.log('changing something for git status')
+console.log('changing something for git diff')
 
